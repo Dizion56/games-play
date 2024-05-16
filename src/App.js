539f3d607f13aae5c2decaf86cdf345a7a76c7b8
Dizion56 <tihomir516@gmail.com>
2024-05-16 15:20:29 +0300

    add routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import Header from "./components/Header/Header.js";
 import "./App.css";
 import Home from "./components/Home/Home.js";
 import { Route, Routes } from "react-router-dom";
+import Login from "./components/Login/Login.js";
+import Register from "./components/Register/Register.js";
+import Catalogue from "./components/Catalogue/Catalogue.js";
+import Create from "./components/Create/Create.js";
 
 function App() {
   return (
@@ -11,6 +15,10 @@ function App() {
       <main id="main-content">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/catalogue" element={<Catalogue />} />
+          <Route path="/create" element={<Create />} />
         </Routes>
       </main>
     </div>
